a3b88db4f3bc96b32bedc3bac945aef304f2312a
Manuel Mesa <manuelmesacano@gmail.com>
2023-10-30 17:34:49 +0100

    delete all labs

---
+++ delete-xf-labs.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+instances=$(gcloud compute instances list --filter=name:xf-lab --format="value(name)")
+
+for instance in $instances; do
+    gcloud compute instances delete "$instance"
+done
