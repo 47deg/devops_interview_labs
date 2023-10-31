b5b5c36e823e9d3511ff4917673660e57bbab18e
Manuel Mesa <manuelmesacano@gmail.com>
2023-10-31 17:20:25 +0100

    no need for sed

--- delete-xf-labs.bash
+++ delete-xf-labs.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-instances=$(gcloud compute instances list --filter=name:xf-lab --format="csv(name,zone.basename())" | sed '1d')
+instances=$(gcloud compute instances list --filter=name:xf-lab --format="csv[no-heading](name,zone.basename())")
 
 echo "$instances" | while IFS=',' read -r col1 col2; do
     gcloud compute instances delete "$col1" --zone "$col2" --quiet
