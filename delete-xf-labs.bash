dd0efbb1534cb77b51ac325c869a2636a118f236
Manuel Mesa <manuelmesacano@gmail.com>
2023-10-31 17:16:51 +0100

    add zone to delete command

--- delete-xf-labs.bash
+++ delete-xf-labs.bash
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-instances=$(gcloud compute instances list --filter=name:xf-lab --format="value(name)")
+instances=$(gcloud compute instances list --filter=name:xf-lab --format="csv(name,zone.basename())" | sed '1d')
 
-for instance in $instances; do
-    # gcloud compute instances delete "$instance"
-    yes Y | gcloud compute instances delete "$instance"
+echo "$instances" | while IFS=',' read -r col1 col2; do
+    gcloud compute instances delete "$col1" --zone "$col2" --quiet
 done
